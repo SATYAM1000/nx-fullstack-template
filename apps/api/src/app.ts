190e053b5548ac486daa2bb3d2e0dc81a7361f1b
Satyam Kumar <satyamsingh748846@gmail.com>
2025-10-13 16:39:46 +0530

    feat(api): Add server start config

---
+++ apps/api/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import compression from 'compression';
+import hpp from 'hpp';
+import cors from 'cors';
+
+import { env, rateLimiter } from './config';
+import {
+  globalErrorMiddleware,
+  requestLoggerMiddleware,
+  routeNotFoundMiddleware,
+  validateJsonMiddleware,
+} from './middlewares';
+
+const app = express();
+
+app.set('trust proxy', 1);
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(hpp());
+app.use(compression());
+app.use(rateLimiter);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(validateJsonMiddleware);
+
+app.use(
+  cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    origin: [env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+
+app.use(requestLoggerMiddleware);
+
+app.use(routeNotFoundMiddleware);
+
+app.use(globalErrorMiddleware);
+
+export default app;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,53 +1,31 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import compression from 'compression';
-import hpp from 'hpp';
-import cors from 'cors';
-
-import { env, rateLimiter } from './config';
+import app from './app';
 import {
-  globalErrorMiddleware,
-  requestLoggerMiddleware,
-  routeNotFoundMiddleware,
-  validateJsonMiddleware,
-} from './middlewares';
+  connectToLogsDB,
+  connectToMainDB,
+  disconnectFromDB,
+  env,
+  initRedisClient,
+  logger,
+  redisClient,
+} from './config';
 
-const host = env.HOST;
-const port = env.PORT;
+(async function startServer() {
+  try {
+    await connectToMainDB();
+    await Promise.all([connectToLogsDB(), initRedisClient()]);
 
-const app = express();
+    app.listen(env.PORT, () => {
+      logger.info(`🚀 Server is up and running in ${env.NODE_ENV} mode`);
+    });
 
-app.set('trust proxy', 1);
-app.use(express.json());
-app.use(cookieParser());
-app.use(helmet());
-app.use(hpp());
-app.use(compression());
-app.use(rateLimiter);
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(validateJsonMiddleware);
-
-app.use(
-  cors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    origin: [env.FRONTEND_URL],
-    credentials: true,
-  })
-);
-
-app.use(requestLoggerMiddleware);
-
-app.use(routeNotFoundMiddleware);
-
-app.use(globalErrorMiddleware);
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+    process.on('SIGINT', async () => {
+      logger.info('👋 Shutting down gracefully...');
+      await disconnectFromDB();
+      await redisClient.quit();
+      process.exit(0);
+    });
+  } catch (error) {
+    logger.error(`❌ Failed to connect to DB: ${error}`);
+    process.exit(1);
+  }
+})();

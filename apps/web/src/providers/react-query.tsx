c2f4f33f99066f2ed2bebb80eba74fbfa03dd262
Satyam Kumar <satyamsingh748846@gmail.com>
2025-10-21 19:56:08 +0530

    fix(web): React query fix

--- apps/web/src/providers/react-query.tsx
+++ apps/web/src/providers/react-query.tsx
@@ -1,7 +1,9 @@
 'use client';
-import React, { ReactNode } from 'react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import React, { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
-  return <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
